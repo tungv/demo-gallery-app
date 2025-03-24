2150667d1a6915eae4de17a05fcc8dd5eeaf263e
Tung Vu <me@tungv.com>
2025-03-24 20:44:50 +0700

    rework home page to add link to creator upload form
    Signed-off-by: Tung Vu <me@tungv.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import { Subtitle, Title } from "@/components/typography";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>hello world</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Title>Gallery App</Title>
+      <Subtitle>Welcome to the Gallery App</Subtitle>
+
+      <Button asChild>
+        <Link href="/creator/upload">Upload</Link>
+      </Button>
+    </div>
+  );
 }
