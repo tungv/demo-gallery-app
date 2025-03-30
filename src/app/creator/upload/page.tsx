eba043374b4e4e67c05cfcafd4e9e60c82bd171d
Tung Vu <me@tungv.com>
2025-03-30 10:05:38 +0700

    apply new form components
    Signed-off-by: Tung Vu <me@tungv.com>

--- src/app/creator/upload/page.tsx
+++ src/app/creator/upload/page.tsx
@@ -3,9 +3,9 @@ import {
   Form,
   FormField,
   FormLabel,
-  FormControl,
   FormMessage,
   FormSubmit,
+  InputControl,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
@@ -14,18 +14,25 @@ export default function UploadPage() {
     <div className="flex flex-col items-center justify-center h-screen p-4">
       <Title className="mb-8">Upload</Title>
 
-      <Form>
+      <Form className="invalid:border-red-500 border">
         <FormField name="image_title">
           <FormLabel>Image Title</FormLabel>
-          <FormControl asChild>
-            <Input required placeholder="Enter image title..." />
-          </FormControl>
-          <FormMessage match="valueMissing">
-            Please enter a title for your image
-          </FormMessage>
+          <InputControl asChild>
+            <Input required placeholder="Enter image title..." minLength={3} />
+          </InputControl>
+          <div className="pile">
+            <FormMessage match="valueMissing">
+              Please enter a title for your image
+            </FormMessage>
+            <FormMessage match="tooShort">
+              Title must be at least 3 characters long
+            </FormMessage>
+            <FormMessage match="valid">&nbsp;</FormMessage>
+          </div>
         </FormField>
 
         <FormSubmit>Upload Image</FormSubmit>
+        <button type="reset">Reset</button>
       </Form>
     </div>
   );
