39b7364bbed719fdd6afd472712ff4c112a4085b
Tung Vu <me@tungv.com>
2025-04-02 22:44:04 +0700

    feat(use-effect-event): impl
    Signed-off-by: Tung Vu <me@tungv.com>

---
+++ src/components/ui/use-effect-event.tsx
@@ -0,0 +1,21 @@
+import { useCallback } from "react";
+
+import { useEffect } from "react";
+
+import { useRef } from "react";
+
+type VoidFunction = (...args: any[]) => void;
+
+export default function useEffectEvent<T extends VoidFunction>(fn: T): T {
+  const ref = useRef<T>(fn);
+
+  useEffect(() => {
+    ref.current = fn;
+  }, [fn]);
+
+  const t = useCallback((...args: Parameters<T>): void => {
+    ref.current(...args);
+  }, []) as T;
+
+  return t;
+}
