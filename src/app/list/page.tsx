1f8ec272a71f6777c6683814a3ea883ed9ec4484
Tung Vu <me@tungv.com>
2025-06-13 18:26:30 +0700

    feat: enhance ListPage layout with padding and add interactive button
    - Added padding to GridListRoot for improved spacing.
    - Introduced an interactive button with an icon in the grid row for adding items.
    - Updated CustomRow styling to include focus outline for better accessibility.
    
    Signed-off-by: Tung Vu <me@tungv.com>

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -12,7 +12,7 @@ export default function ListPage() {
   return (
     <div className="bg-muted p-12">
       <Button>Before</Button>
-      <GridListRoot gridColumnTemplate="1fr auto 1fr" className="bg-white">
+      <GridListRoot gridColumnTemplate="1fr auto 1fr" className="bg-white p-2">
         <GridListHeader>header</GridListHeader>
         <GridListBody>
           <GridListRow asChild rowId="1">
@@ -21,10 +21,18 @@ export default function ListPage() {
           <GridListRow asChild rowId="2">
             <CustomRow />
           </GridListRow>
-          <GridListRow rowId="3">
+          <GridListRow
+            rowId="3"
+            className="data-[focused=true]:outline outline-secondary"
+          >
             <div>1</div>
             <div>2</div>
-            <div>3</div>
+            <div>
+              <Button variant="outline">
+                <PlusIcon />
+                <span>Add</span>
+              </Button>
+            </div>
           </GridListRow>
         </GridListBody>
         <GridListFooter>footer</GridListFooter>
@@ -36,7 +44,7 @@ export default function ListPage() {
 
 function CustomRow() {
   return (
-    <div className="items-center">
+    <div className="items-center data-[focused=true]:outline outline-primary">
       <h2>row title</h2>
 
       <span>10,000,000</span>
