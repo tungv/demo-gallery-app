c5c2ff4bcf1328ff64165713673f62e107fd7f86
Tung Vu <me@tungv.com>
2025-06-17 12:05:27 +0700

    feat: add FocusVisibleDebugger component for visual focus tracking
    - Introduced FocusVisibleDebugger component to visually indicate focus visibility state.
    - Utilizes useFocusVisible hook to determine focus status and displays it with color-coded indicators.
    
    Signed-off-by: Tung Vu <me@tungv.com>

---
+++ src/app/list/FocusVisibleDebugger.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useFocusVisible } from "@/components/ui/use-focus-visible";
+import { cn } from "@/lib/utils";
+
+export default function FocusVisibleDebugger() {
+  const { isFocusVisible } = useFocusVisible();
+  return (
+    <dl className="w-fit flex flex-row gap-2 items-center">
+      <dt>focusVisible</dt>
+      <dd>
+        <div
+          className={cn(
+            "size-4 rounded-full",
+            isFocusVisible ? "bg-green-500" : "bg-red-500",
+          )}
+        />
+      </dd>
+    </dl>
+  );
+}
