cd679a5563b3a21da9b7e79b22d349d646158e1c
Tung Vu <me@tungv.com>
2025-06-17 12:05:39 +0700

    feat: update ListPage layout and integrate FocusVisibleDebugger
    - Added FocusVisibleDebugger component to ListPage for enhanced focus visibility tracking.
    - Adjusted layout styles to include height adjustments for better responsiveness.
    
    Signed-off-by: Tung Vu <me@tungv.com>

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -10,11 +10,13 @@ import {
   GridListRow,
 } from "@/components/ui/grid-list";
 import { PlusIcon, TrashIcon } from "lucide-react";
+import FocusVisibleDebugger from "./FocusVisibleDebugger";
 
 export default function ListPage() {
   return (
-    <div className="bg-muted grid grid-cols-1 gap-12 p-12">
+    <div className="bg-muted grid grid-cols-1 gap-12 p-12 h-dvh">
       <a href="#1">1</a>
+      <FocusVisibleDebugger />
       <GridListRoot className="bg-white p-2 rounded-lg data-[focus-visible=true]:outline-2 outline-primary grid-cols-[auto_1fr_auto]">
         <GridListHeader className="p-1 gap-x-8">
           <h3 className="text-sm font-medium">Title</h3>
