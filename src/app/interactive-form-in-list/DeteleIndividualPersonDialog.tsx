5c3dc82b782ecf1a14eaa221319dc8abed0ce65b
Tung Vu <me@tungv.com>
2025-06-19 19:23:13 +0700

    extract delete dialog and remove form component
    Signed-off-by: Tung Vu <me@tungv.com>

---
+++ src/app/interactive-form-in-list/DeteleIndividualPersonDialog.tsx
@@ -0,0 +1,65 @@
+import {
+  InteractiveForm,
+  LoadingMessage,
+  SubmitButton,
+  SubmitMessage,
+  Success,
+} from "@/components/behaviors/interactive-form";
+import { DialogClose } from "@/components/ui/dialog";
+import {
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { GridCurrentFocusInput } from "@/components/ui/grid-list";
+import { deletePersonById } from "./actions";
+import { Button } from "@/components/ui/button";
+import { Trash2 } from "lucide-react";
+import { ReserveLayout } from "@/components/ui/reserve-layout";
+import { AutoCloseDialog } from "./PeopleListDialog";
+
+export default function DeleteIndividualPersonDialog() {
+  return (
+    <InteractiveForm
+      className="contents"
+      action={async (formData) => {
+        "use server";
+        const selected = formData.get("deleting-id") as string;
+        await deletePersonById(selected);
+        return { result: "success" };
+      }}
+    >
+      <DialogHeader>
+        <DialogTitle>Delete person?</DialogTitle>
+        <DialogDescription>
+          Are you sure you want to delete this person?
+        </DialogDescription>
+
+        <GridCurrentFocusInput name="deleting-id" />
+
+        <Success>
+          <AutoCloseDialog />
+        </Success>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline" size="sm">
+              Cancel
+            </Button>
+          </DialogClose>
+
+          <SubmitButton asChild>
+            <Button variant="destructive" size="sm">
+              <Trash2 className="size-4" />
+              <ReserveLayout>
+                <LoadingMessage>Deleting...</LoadingMessage>
+                <SubmitMessage>Delete person</SubmitMessage>
+              </ReserveLayout>
+            </Button>
+          </SubmitButton>
+        </DialogFooter>
+      </DialogHeader>
+    </InteractiveForm>
+  );
+}
--- src/app/interactive-form-in-list/page.tsx
+++ src/app/interactive-form-in-list/page.tsx
@@ -35,6 +35,7 @@ import {
   FormBoundary,
   InteractiveForm,
   LoadingMessage,
+  SubmitButton,
   SubmitMessage,
 } from "@/components/behaviors/interactive-form";
 import { deletePeopleByIds, deletePersonById } from "./actions";
@@ -53,7 +54,8 @@ import {
   PeopleListDialogContent,
   PeopleListDialogProvider,
   PeopleListDialogTrigger,
-} from "./state";
+} from "./PeopleListDialog";
+import DeleteIndividualPersonDialog from "./DeteleIndividualPersonDialog";
 
 interface Person {
   id: string;
@@ -70,198 +72,161 @@ export default async function InteractiveFormInList() {
   const people = await getPeople();
   return (
     <div className="bg-muted grid grid-cols-1 gap-12 p-12 h-dvh">
-      <Form className="contents" asChild>
-        <InteractiveForm>
-          <PeopleListDialogProvider>
-            <GridListRoot
-              className="bg-white rounded-lg grid-cols-[auto_auto_1fr_auto_auto_auto_auto_auto_auto] h-fit"
-              selectionMode="multiple"
-              name="people-list"
-            >
-              <GridListTitle className="p-2">People</GridListTitle>
+      <InteractiveForm className="contents">
+        <PeopleListDialogProvider>
+          <GridListRoot
+            className="bg-white rounded-lg grid-cols-[auto_auto_1fr_auto_auto_auto_auto_auto_auto] h-fit"
+            selectionMode="multiple"
+            name="people-list"
+          >
+            <GridListTitle className="p-2">People</GridListTitle>
 
-              {people.length === 0 ? (
-                <>
-                  <GridListCaption className="px-2 pb-2">
-                    This list is empty.
-                  </GridListCaption>
-                  <div className="p-12 text-center col-span-full bg-muted">
-                    <p className="text-muted-foreground italic">
-                      No people found.
-                    </p>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <GridListCaption className="px-2 pb-2">
-                    Showing {people.length} people.
-                  </GridListCaption>
-                  <GridListHeader>
-                    <GridListRow>
-                      <PeopleHeaderCell>
+            {people.length === 0 ? (
+              <>
+                <GridListCaption className="px-2 pb-2">
+                  This list is empty.
+                </GridListCaption>
+                <div className="p-12 text-center col-span-full bg-muted">
+                  <p className="text-muted-foreground italic">
+                    No people found.
+                  </p>
+                </div>
+              </>
+            ) : (
+              <>
+                <GridListCaption className="px-2 pb-2">
+                  Showing {people.length} people.
+                </GridListCaption>
+                <GridListHeader>
+                  <GridListRow>
+                    <PeopleHeaderCell>
+                      <CheckBox />
+                    </PeopleHeaderCell>
+                    <PeopleHeaderCell>Name</PeopleHeaderCell>
+                    <PeopleHeaderCell>Email</PeopleHeaderCell>
+                    <PeopleHeaderCell>Phone</PeopleHeaderCell>
+                    <PeopleHeaderCell>Address</PeopleHeaderCell>
+                    <PeopleHeaderCell>City</PeopleHeaderCell>
+                    <PeopleHeaderCell>State</PeopleHeaderCell>
+                    <PeopleHeaderCell>Zip</PeopleHeaderCell>
+                    <PeopleHeaderCell>Actions</PeopleHeaderCell>
+                  </GridListRow>
+                </GridListHeader>
+                <GridListBody className="divide-y border-y">
+                  {people.map((person) => (
+                    <GridListRow
+                      key={person.id}
+                      className="items-center gap-4 focus-visible:outline-2 outline-primary rounded-md focus-within:bg-secondary hover:bg-accent group"
+                      rowId={person.id}
+                      rowData={person}
+                    >
+                      <GridListCell className="p-1">
                         <CheckBox />
-                      </PeopleHeaderCell>
-                      <PeopleHeaderCell>Name</PeopleHeaderCell>
-                      <PeopleHeaderCell>Email</PeopleHeaderCell>
-                      <PeopleHeaderCell>Phone</PeopleHeaderCell>
-                      <PeopleHeaderCell>Address</PeopleHeaderCell>
-                      <PeopleHeaderCell>City</PeopleHeaderCell>
-                      <PeopleHeaderCell>State</PeopleHeaderCell>
-                      <PeopleHeaderCell>Zip</PeopleHeaderCell>
-                      <PeopleHeaderCell>Actions</PeopleHeaderCell>
+                      </GridListCell>
+                      <GridListRowHeader className="font-medium text-left p-1">
+                        {person.name}
+                      </GridListRowHeader>
+                      <GridListCell className="p-1">
+                        <span className="select-all">{person.email}</span>
+                      </GridListCell>
+                      <GridListCell className="p-1 tabular-nums">
+                        <span className="select-all">{person.phone}</span>
+                      </GridListCell>
+                      <GridListCell className="p-1">
+                        {person.address}
+                      </GridListCell>
+                      <GridListCell className="p-1">{person.city}</GridListCell>
+                      <GridListCell className="p-1">
+                        {person.state}
+                      </GridListCell>
+                      <GridListCell className="p-1 tabular-nums">
+                        <span className="select-all">{person.zip}</span>
+                      </GridListCell>
+                      <GridListCell className="p-1">
+                        <ActionsCell />
+                      </GridListCell>
                     </GridListRow>
-                  </GridListHeader>
-                  <GridListBody className="divide-y border-y">
-                    {people.map((person) => (
-                      <GridListRow
-                        key={person.id}
-                        className="items-center gap-4 focus-visible:outline-2 outline-primary rounded-md focus-within:bg-secondary hover:bg-accent group"
-                        rowId={person.id}
-                        rowData={person}
-                      >
-                        <GridListCell className="p-1">
-                          <CheckBox />
-                        </GridListCell>
-                        <GridListRowHeader className="font-medium text-left p-1">
-                          {person.name}
-                        </GridListRowHeader>
-                        <GridListCell className="p-1">
-                          <span className="select-all">{person.email}</span>
-                        </GridListCell>
-                        <GridListCell className="p-1 tabular-nums">
-                          <span className="select-all">{person.phone}</span>
-                        </GridListCell>
-                        <GridListCell className="p-1">
-                          {person.address}
-                        </GridListCell>
-                        <GridListCell className="p-1">
-                          {person.city}
-                        </GridListCell>
-                        <GridListCell className="p-1">
-                          {person.state}
-                        </GridListCell>
-                        <GridListCell className="p-1 tabular-nums">
-                          <span className="select-all">{person.zip}</span>
-                        </GridListCell>
-                        <GridListCell className="p-1">
-                          <ActionsCell />
-                        </GridListCell>
-                      </GridListRow>
-                    ))}
-                  </GridListBody>
-                </>
-              )}
+                  ))}
+                </GridListBody>
+              </>
+            )}
 
-              <GridListFooter>
-                <GridListRow className="p-2 gap-4 flex flex-row">
-                  {/* FormBoundary is here to ensure the form is reset after a successful submission */}
-                  <FormBoundary>
-                    <AddPersonDialog />
-                  </FormBoundary>
-                  <NonEmptySelection minSize={2}>
-                    <Dialog>
-                      <DialogTrigger asChild>
-                        <Button variant="destructive" size="sm" type="button">
-                          <Trash2 className="size-4" />
-                          <ReserveLayout>
-                            <LoadingMessage>Deleting...</LoadingMessage>
-                            <SubmitMessage>Delete multiple</SubmitMessage>
-                          </ReserveLayout>
-                        </Button>
-                      </DialogTrigger>
+            <GridListFooter>
+              <GridListRow className="p-2 gap-4 flex flex-row">
+                {/* FormBoundary is here to ensure the form is reset after a successful submission */}
+                <FormBoundary>
+                  <AddPersonDialog />
+                </FormBoundary>
+                <NonEmptySelection minSize={2}>
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Button variant="destructive" size="sm" type="button">
+                        <Trash2 className="size-4" />
+                        <ReserveLayout>
+                          <LoadingMessage>Deleting...</LoadingMessage>
+                          <SubmitMessage>Delete multiple</SubmitMessage>
+                        </ReserveLayout>
+                      </Button>
+                    </DialogTrigger>
 
-                      <DialogContent>
-                        <DialogHeader>
-                          <DialogTitle>Delete multiple people</DialogTitle>
-                        </DialogHeader>
-                        <DialogDescription>
-                          Are you sure you want to delete the selected people?
-                        </DialogDescription>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Delete multiple people</DialogTitle>
+                      </DialogHeader>
+                      <DialogDescription>
+                        Are you sure you want to delete the selected people?
+                      </DialogDescription>
 
-                        <p>People to delete:</p>
-                        <SelectedPeopleNameList />
+                      <p>People to delete:</p>
+                      <SelectedPeopleNameList />
 
-                        <DialogFooter>
-                          <DialogClose asChild>
-                            <Button variant="outline" size="sm">
-                              Cancel
-                            </Button>
-                          </DialogClose>
+                      <DialogFooter>
+                        <DialogClose asChild>
+                          <Button variant="outline" size="sm">
+                            Cancel
+                          </Button>
+                        </DialogClose>
 
-                          <ActionButton<"people-list">
-                            asChild
-                            formAction={async (formData) => {
-                              "use server";
-                              const selected = formData.getAll(
-                                "people-list",
-                              ) as string[];
-                              await deletePeopleByIds(selected);
-                              return {
-                                refresh: true,
-                              };
-                            }}
+                        <ActionButton<"people-list">
+                          asChild
+                          formAction={async (formData) => {
+                            "use server";
+                            const selected = formData.getAll(
+                              "people-list",
+                            ) as string[];
+                            await deletePeopleByIds(selected);
+                            return {
+                              refresh: true,
+                            };
+                          }}
+                        >
+                          <Button
+                            variant="destructive"
+                            size="sm"
+                            className="gap-2"
                           >
-                            <Button
-                              variant="destructive"
-                              size="sm"
-                              className="gap-2"
-                            >
-                              <Trash2 className="size-4" />
-                              <ReserveLayout>
-                                <LoadingMessage>Deleting...</LoadingMessage>
-                                <SubmitMessage>Delete multiple</SubmitMessage>
-                              </ReserveLayout>
-                            </Button>
-                          </ActionButton>
-                        </DialogFooter>
-                      </DialogContent>
-                    </Dialog>
-                  </NonEmptySelection>
-                </GridListRow>
-              </GridListFooter>
+                            <Trash2 className="size-4" />
+                            <ReserveLayout>
+                              <LoadingMessage>Deleting...</LoadingMessage>
+                              <SubmitMessage>Delete multiple</SubmitMessage>
+                            </ReserveLayout>
+                          </Button>
+                        </ActionButton>
+                      </DialogFooter>
+                    </DialogContent>
+                  </Dialog>
+                </NonEmptySelection>
+              </GridListRow>
+            </GridListFooter>
 
-              <PeopleListDialog>
-                <PeopleListDialogContent when="delete-person">
-                  <DialogHeader>
-                    <DialogTitle>Delete person?</DialogTitle>
-                    <DialogDescription>
-                      Are you sure you want to delete this person?
-                    </DialogDescription>
-
-                    <DialogFooter>
-                      <DialogClose asChild>
-                        <Button variant="outline" size="sm">
-                          Cancel
-                        </Button>
-                      </DialogClose>
-
-                      <ActionButton<"people-list.focused">
-                        asChild
-                        formAction={async (formData) => {
-                          "use server";
-                          const selected = formData.get(
-                            "people-list.focused",
-                          ) as string;
-                          await deletePersonById(selected);
-                          return { refresh: true };
-                        }}
-                      >
-                        <Button type="button" variant="destructive" size="sm">
-                          <Trash2 className="size-4" />
-                          <ReserveLayout>
-                            <LoadingMessage>Deleting...</LoadingMessage>
-                            <SubmitMessage>Delete person</SubmitMessage>
-                          </ReserveLayout>
-                        </Button>
-                      </ActionButton>
-                    </DialogFooter>
-                  </DialogHeader>
-                </PeopleListDialogContent>
-              </PeopleListDialog>
-            </GridListRoot>
-          </PeopleListDialogProvider>
-        </InteractiveForm>
-      </Form>
+            <PeopleListDialog>
+              <PeopleListDialogContent when="delete-person">
+                <DeleteIndividualPersonDialog />
+              </PeopleListDialogContent>
+            </PeopleListDialog>
+          </GridListRoot>
+        </PeopleListDialogProvider>
+      </InteractiveForm>
     </div>
   );
 }
