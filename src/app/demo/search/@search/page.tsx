7da0d8e09daf9564ff48e93e74e5df81872a083b
Tung Vu <me@tungv.com>
2025-06-29 17:14:57 +0700

    render search results
    Signed-off-by: Tung Vu <me@tungv.com>

--- src/app/demo/search/@search/page.tsx
+++ src/app/demo/search/@search/page.tsx
@@ -1,39 +1,69 @@
+import Link from "next/link";
+
 interface SearchResultsProps {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
-export default async function SearchResults({
+export default async function SearchResultsPage({
   searchParams,
 }: SearchResultsProps) {
   const params = await searchParams;
   const query = params.q as string | undefined;
 
   if (!query) {
-    return null;
+    return <RecommendedSearches />;
   }
 
+  return <SearchResults keyword={query} />;
+}
+
+function RecommendedSearches() {
+  const RECOMMENDED_SEARCHES = [
+    "javascript tutorials",
+    "react tutorials",
+    "node.js tutorials",
+  ];
+  return (
+    <div className="grid grid-cols-1 gap-4 bg-white rounded-lg p-6 border">
+      <h2 className="text-xl font-semibold">Recommended Searches</h2>
+
+      <div className="grid grid-cols-1 gap-3">
+        {RECOMMENDED_SEARCHES.map((search) => (
+          <Link href={`/demo/search?q=${search}`} key={search}>
+            {search}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function SearchResults({ keyword }: { keyword: string }) {
   // Simulate search results
   const results = [
     {
       id: 1,
-      title: `Result for "${query}" - Item 1`,
+      title: `Result for "${keyword}" - Item 1`,
       description: "Sample description for the first search result.",
     },
     {
       id: 2,
-      title: `Result for "${query}" - Item 2`,
+      title: `Result for "${keyword}" - Item 2`,
       description: "Sample description for the second search result.",
     },
     {
       id: 3,
-      title: `Result for "${query}" - Item 3`,
+      title: `Result for "${keyword}" - Item 3`,
       description: "Sample description for the third search result.",
     },
   ];
 
   return (
     <div className="grid grid-cols-1 gap-4 bg-white rounded-lg p-6 border">
-      <h2 className="text-xl font-semibold">Search Results for "{query}"</h2>
+      <h2 className="text-xl font-semibold">
+        Search Results for &quot;{keyword}&quot;
+      </h2>
+
       <div className="grid grid-cols-1 gap-3">
         {results.map((result) => (
           <div
