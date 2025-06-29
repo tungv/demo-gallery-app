d20a8c56e294180ddc6c2e01c0841a58abc0e7f8
Tung Vu <me@tungv.com>
2025-06-29 22:53:30 +0700

    use GridList for recommended search
    Signed-off-by: Tung Vu <me@tungv.com>

--- src/app/demo/search/@search/results/RecommendedSearches.tsx
+++ src/app/demo/search/@search/results/RecommendedSearches.tsx
@@ -1,4 +1,13 @@
 import { NavigationButton } from "@/components/behaviors/navigation-form";
+import {
+  GridListContainer,
+  GridListContent,
+  GridBody,
+  GridListRow,
+  GridListCell,
+  GridListRowHeader,
+} from "@/components/ui/grid-list";
+import { Search } from "lucide-react";
 
 export default function RecommendedSearches() {
   const RECOMMENDED_SEARCHES = [
@@ -6,20 +15,35 @@ export default function RecommendedSearches() {
     "react tutorials",
     "node.js tutorials",
   ];
-  return (
-    <div className="grid grid-cols-1 gap-4 bg-white rounded-lg p-6 border">
-      <h2 className="text-xl font-semibold">Recommended Searches</h2>
 
-      <div className="grid grid-cols-1 gap-3">
-        {RECOMMENDED_SEARCHES.map((search) => (
-          <NavigationButton
-            searchParams={new URLSearchParams({ q: search })}
-            key={search}
-          >
-            {search}
-          </NavigationButton>
-        ))}
-      </div>
+  return (
+    <div className="bg-white rounded-lg p-6 border">
+      <h2 className="text-xl font-semibold mb-4">Recommended Searches</h2>
+
+      <GridListContainer selectionMode="none">
+        <GridListContent gridClassName="grid grid-cols-[1fr_auto] gap-2 p-1">
+          <GridBody>
+            {RECOMMENDED_SEARCHES.map((search) => (
+              <GridListRow
+                key={search}
+                rowId={search}
+                className="hover:bg-accent p-2 rounded-md focus-visible:outline-2 outline-primary focus-visible:bg-accent items-center"
+                asChild
+              >
+                <NavigationButton
+                  searchParams={new URLSearchParams({ q: search })}
+                >
+                  <GridListRowHeader>{search}</GridListRowHeader>
+                  <GridListCell>
+                    <Search className="size-4" />
+                    <span className="sr-only">Search for {search}</span>
+                  </GridListCell>
+                </NavigationButton>
+              </GridListRow>
+            ))}
+          </GridBody>
+        </GridListContent>
+      </GridListContainer>
     </div>
   );
 }
